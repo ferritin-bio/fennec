85347be0066da89bca25dd18a0cef46ca2edb7a8
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2025-04-04 10:28:40 -0400

    Begin porting Bioinformatics toolkit over.

---
+++ src-tauri/src/commands/dna_utils.rs
@@ -0,0 +1,17 @@
+// use std::io::prelude::*;
+use protein_translate::translate;
+
+//  DNA Utilities -------------------------------------------------------------------------------------------------------------------
+
+#[tauri::command]
+pub fn translate_dna(dnainput: String) -> String {
+    translate(dnainput.as_bytes())
+}
+
+// https://github.com/SecureDNA/quickdna/blob/main/src/rust_api.rs#L410
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tempfile::NamedTempFile;
+}
--- src-tauri/src/commands/mod.rs
+++ src-tauri/src/commands/mod.rs
@@ -1,3 +1,7 @@
 pub(crate) mod amplify;
+pub mod dna_utils;
 pub(crate) mod esm2_logits;
+pub mod fastx;
 pub(crate) mod ligandmpnn_logits;
+pub mod patterns;
+pub mod rnapkin;
