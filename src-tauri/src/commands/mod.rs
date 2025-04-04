cd440e75b4d98ddccbce1753b631ae96c66dc423
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2025-04-04 10:36:21 -0400

    add all the fns

--- src-tauri/src/commands/mod.rs
+++ src-tauri/src/commands/mod.rs
@@ -1,7 +1,7 @@
 pub(crate) mod amplify;
-pub mod dna_utils;
+pub(crate) mod dna_utils;
 pub(crate) mod esm2_logits;
-pub mod fastx;
+pub(crate) mod fastx;
 pub(crate) mod ligandmpnn_logits;
-pub mod patterns;
-pub mod rnapkin;
+pub(crate) mod patterns;
+pub(crate) mod rnapkin;
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,9 +3,13 @@
 // This namespace exists to define the handlers we pass to Tauri.
 //
 mod commands;
+use commands::amplify::{get_amplify_contact_map, get_amplify_logits};
+use commands::dna_utils::translate_dna;
 use commands::esm2_logits::get_esm2_logits;
+use commands::fastx::{convert_fastq_to_fasta_tauri, get_seqstats, get_stats};
 use commands::ligandmpnn_logits::get_ligmpnn_logits;
-use commands::amplify::{get_amplify_logits,get_amplify_contact_map};
+use commands::patterns::check_restriction_sites;
+use commands::rnapkin::rnapkin_fn;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -16,6 +20,11 @@ pub fn run() {
             get_esm2_logits,
             get_amplify_logits,
             get_amplify_contact_map,
+            rnapkin_fn,
+            check_restriction_sites,
+            convert_fastq_to_fasta_tauri,
+            get_seqstats,
+            get_stats,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
